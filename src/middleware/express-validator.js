042b27ee280a2d0a6a00d321e940d3f62f22b0c8
barrionuevoJ <julianbarrionuevo10@gmail.com>
2023-03-15 03:37:16 -0300

    acomodando un poco el formulario de creacion

--- src/middleware/express-validator.js
+++ src/middleware/express-validator.js
@@ -7,9 +7,9 @@ const validaciones = {
         body('autor').isLength({ min: 1 }).withMessage('El autor es obligatorio.').bail(),
         body('descripcion').isLength({ min: 1 }).withMessage('La descripcion es obligatoria.').bail(),
         body('categoria').isLength({ min: 1 }).withMessage('La categoria es obligatoria.').bail(),
-        body('descuento').isNumeric().default('0').withMessage('El descuento debe ser un numero.').bail(),
-        body('cantidad').isNumeric().default('1').withMessage('La cantidad debe ser un numero.').bail(),
-        body('precio').isNumeric().default('0').withMessage('El precio debe ser un numero.').bail(),
+        body('descuento').isNumeric().default(0).withMessage('El descuento debe ser un numero.').bail(),
+        body('cantidad').isNumeric().default(1).withMessage('La cantidad debe ser un numero.').bail(),
+        body('precio').isNumeric().default(0).withMessage('El precio debe ser un numero.').bail(),
         body('genero').isLength({ min: 1 }).withMessage('El genero es obligatorio.').bail(),
         body('img').custom((value, { req }) => {
             let file = req.file;
